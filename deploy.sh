90c4f6d875649fa7c57e8fb7812b1956b109d5bf
Jesús Sanz <jesussanz77@gmail.com>
2019-10-22 18:06:34 +0200

    netlify config

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+# deploy.sh
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Linting..
+npm run lint
+echo Building. this may take a minute...
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'example.com' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:<jesussanzdev>/<feg-forum>.git master:gh-pages
+
+cd -
